d64a0a53b2ee8b13edf73386c40114cbddcd8333
SofianeBel <118746369+SofianeBel@users.noreply.github.com>
2023-01-23 05:56:30 +0100

    Add files via upload

---
+++ bienvenue.js
@@ -0,0 +1,7 @@
+<script>
+    // On utilise la méthode setTimeout pour afficher le message de bienvenue
+    setTimeout(function(){
+        // On utilise la méthode alert pour afficher le message de bienvenue
+        alert("Bienvenue sur notre site !");
+    }, 3000); // 3000ms = 3s
+</script>
